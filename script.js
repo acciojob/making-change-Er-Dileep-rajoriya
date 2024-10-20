3f645d565ed23c14b644ac6c87065e821f149b4f
Dileep Rajoriya <139870968+Er-Dileep-rajoriya@users.noreply.github.com>
2024-10-21 00:53:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,32 @@
 const makeChange = (c) => {
-  // your name here
+  let change = {
+    q: 0, // quarters
+    d: 0, // dimes
+    n: 0, // nickels
+    p: 0  // pennies
+  };
+
+  let remainingCents = c; // The remaining amount in cents
+
+  // Quarters
+  change.q = Math.floor(remainingCents / 25);
+  remainingCents = remainingCents % 25;
+
+  // Dimes
+  change.d = Math.floor(remainingCents / 10);
+  remainingCents = remainingCents % 10;
+
+  // Nickels
+  change.n = Math.floor(remainingCents / 5);
+  remainingCents = remainingCents % 5;
+
+  // Pennies
+  change.p = remainingCents;
+
+  return change;
 };
 
-// Do not the change the code below
+// Do not change the code below
 const c = prompt("Enter c: ");
 alert(JSON.stringify(makeChange(c)));
+
